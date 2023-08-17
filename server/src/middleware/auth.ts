92f4ebdc5cad67ee992d5012d3c5ca2f38338379
“Nivin <nivin.john@oracle.com>
2023-08-17 23:53:35 +0530

    backend - admin 1

--- server/src/middleware/auth.ts
+++ server/src/middleware/auth.ts
@@ -3,16 +3,20 @@ import 'dotenv/config';
 import {NextFunction, Request, Response} from "express";
 
 const SECRET = process.env.SECRET;
+// Define an interface that extends the Request interface
+interface CustomRequest extends Request {
+    user?: any; // Change 'any' to the actual type of 'user' if known
+}
 
-const authenticateJwt = (req: Request, res: Response, next: NextFunction) => {
+const authenticateJwt = (req: CustomRequest, res: Response, next: NextFunction) => {
     const authHeader = req.headers.authorization;
     if(authHeader){
         const token = authHeader.split(' ')[1];
-        jwt.verify(token, SECRET, (err, user) => {
+        jwt.verify(token, SECRET, (err, decoded) => {
             if (err) {
                 return res.sendStatus(403);
             }
-            req.user = user;
+            req.user = decoded;
             next();
         });
     } else {
@@ -20,4 +24,4 @@ const authenticateJwt = (req: Request, res: Response, next: NextFunction) => {
     }
 }
 
-module.exports = { authenticateJwt, SECRET };
+export { authenticateJwt, SECRET, CustomRequest };
--- server/src/routes/admin.ts
+++ server/src/routes/admin.ts
@@ -1,25 +1,46 @@
-import {jwt} from "jsonwebtoken";
 import * as express from "express";
-import {SECRET, authenticateJwt} from "../middleware"
-import {Admin, Product} from "../db/models";
-
 import {Request, Response} from "express";
+import * as jwt from "jsonwebtoken";
+import {SignupParams, signupProps} from "@nivdev/common";
+import {Admin, User} from "../db/models";
+import {SECRET, authenticateJwt} from "../middleware/auth"
 
 const router = express.Router();
 
-interface CreateTodoInput{
-    title: string,
-    description: string
-}
+// Admin routes
+router.get('/me', authenticateJwt, (req: Request, res: Response) => {
+    res.json({
+        username: req.user.username
+    })
+});
 
-
-
-router.post("/signup", async (req:Request, res: Response) =>{
+router.post("/signup", async (req: Request, res: Response) => {
     const parsedInput = signupProps.safeParse(req.body);
-    if(!parsedInput.success){
+    if (!parsedInput.success) {
         res.status(411).json({
-            error: parsedInput.error
+            message: parsedInput.error.issues[0].message
         })
         return;
     }
+    const signupParams: SignupParams = {
+        username: parsedInput.data.username,
+        password: parsedInput.data.password
+    }
+    const user = await Admin.findOne({username: signupParams.username});
+    if (user) {
+        res.status(403).json({
+            message: "Admin already exists"
+        })
+    } else {
+        const newUser = new User({username: signupParams.username, password: signupParams.password});
+        await newUser.save();
+        const token = jwt.sign({
+            username: signupParams.username,
+            role: "admin"
+        }, SECRET, {
+            expiresIn: "1h"
+        });
+        res.json({ message: "Admin created successfully", token });
+    }
+
 })
